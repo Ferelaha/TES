169a8307e2bf1269d96439dd268ef11d3db5b878
Ferelaha <158261306+Ferelaha@users.noreply.github.com>
2024-01-31 11:44:41 +0000

    Add files via upload

---
+++ compute.py
@@ -0,0 +1,7 @@
+def computeMultplesSum(n ):
+    sum=0
+    while  n%3=0 ||  n%5=0 || n%7=0;
+    if n/3=a>0 & a<n;
+     sum+=a
+     return(sum)
+def computeMultplesSum()
---
+++ fonction cal.py
@@ -0,0 +1,12 @@
+def calc(array,n1,n2)
+    if (0<n1<n2<array.length):
+    som=0
+        for i in (n1;n2):
+            som+=array[i]
+            i+=1
+            return(som)
+    else:
+        print("l'indice n'est pas prit en compte")
+
+def calc()
+
